800deb03fe0287045e2606c2760e1496d3356b76
Jyoti <jyoti.contact@gmail.com>
2020-03-07 15:24:06 +0000

    basic app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import './App.css';
 
+const animals = [
+  {
+    id: 1,
+    name: 'Louis',
+    species: 'dog'
+    // breed: ,
+    // colour: ,
+    // sex: female,
+    // companionship: 5,
+    // child_friendly: ,
+    // animal_friendly: ,
+    // garden_required:
+  },
+  {
+    id: 2,
+    name: 'Ozzie',
+    species: 'dog'
+  },
+  {
+    id: 3,
+    name: 'Pepper',
+    species: 'dog'
+  }
+];
+
+const info = animal => {
+  return (
+    <div>
+      {animal.name}: {animal.species}
+      {/* <p>{animal.species}</p> */}
+    </div>
+  );
+};
 class App extends React.Component {
-
   render() {
-    return (
-      
-
-    );
+    return <div>{animals.map(info)}</div>;
   }
 }
 
