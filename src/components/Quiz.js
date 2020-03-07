357628013b55ea09726959a4abff71631b821810
Laura Miller <lmiller.spence@gmail.com>
2020-03-07 17:02:18 +0000

    Added quiz component

---
+++ src/components/Quiz.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Question from '../components/Question';
+import QuestionCount from '../components/QuestionCount';
+import AnswerOptions from '../components/AnswerOptions';
+
+  export default Quiz;
