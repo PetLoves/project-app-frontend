664909ffbb1c5df9b4619b59e6c0ae706568b72c
Laura Miller <lmiller.spence@gmail.com>
2020-03-07 17:24:18 +0000

    Consolidated questions and answers into a single array

--- src/App.js
+++ src/App.js
@@ -29,58 +29,45 @@ const animals = [
 
 const questions = [
   {
-    id: 1,
-    question: "Do you have a garden?"
+    question: "Do you have a garden?",
+    answers: [
+      "Yes",
+      "No"
+    ]
   },
   {
-    id: 2,
-    question: "Will I be sharing your home with children?"
+    question: "Will I be sharing your home with children?",
+    answers: [
+      "Yes",
+      "No"
+    ]
   },
   {
-    id: 3,
-    question: "How much training can you commit to?"
+    question: "How much training can you commit to?",
+    answers: [
+      "I'm willing to tackle behavioral issues such as anxiety and leash aggression",
+      "Some house-training and basic commands are fine",
+      "I'd prefer to not bother with training"
+    ]
   },
   {
-    id: 4,
-    question: "How often will I be left home alone?"
+    question: "How often will I be left home alone?",
+    answers: [
+      "0-10 hours per week",
+      "10-25 hours per week",
+      "25-40 hours per week"
+    ]
   },
   {
-    id: 5,
-    question: "What's your activity level?"
+    question: "What's your activity level?",
+    answers: [
+      "I go running most days and hiking at the weekend",
+      "Daily walks around 2-4 miles are great",
+      "I like to potter around in the garden or occassionally go for a walk",
+      "I prefer or am unable to leave the house much."
+    ]
   }
-]
-
-const answers = [
-  {
-    id: 1,
-    answer0: "Yes",
-    answer1: "No" 
-  },
-  {
-    id: 2,
-    answer0: "Yes",
-    answer1: "No"
-  },
-  {
-    id: 3,
-    answer0: "I'm willing to tackle behavioral issues such as anxiety and leash aggression",
-    answer1: "Some house-training and basic commands are fine",
-    answer2: "I'd prefer to not bother with training"
-  },
-  {
-    id: 4,
-    answer0: "0-10 hours per week",
-    answer1: "10-25 hours per week",
-    answer2: "25-40 hours per week"
-  },
-  {
-    id: 5,
-    answer0: "I go running most days and hiking at the weekend",
-    answer1: "Daily walks around 2-4 miles are great",
-    answer2: "I like to potter around in the garden or occassionally go for a walk",
-    answer3: "I prefer or am unable to leave the house much."
-  }
-]
+];
 
 const info = animal => {
   return (
