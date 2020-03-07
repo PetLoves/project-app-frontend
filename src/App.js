de53c8f25aa3ae78d0ec8e58dc34200b78d776fc
Laura Miller <lmiller.spence@gmail.com>
2020-03-07 16:37:43 +0000

    Fixed syntax for objects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Question from './components/Question';
 
 const animals = [
   {
@@ -30,19 +31,19 @@ const questions = [
   {
     id: 1,
     question: "Do you have a garden?"
-  }
+  },
   {
     id: 2,
     question: "Will I be sharing your home with children?"
-  }
+  },
   {
     id: 3,
     question: "How much training can you commit to?"
-  }
+  },
   {
     id: 4,
     question: "How often will I be left home alone?"
-  }
+  },
   {
     id: 5,
     question: "What's your activity level?"
@@ -54,24 +55,24 @@ const answers = [
     id: 1,
     answer0: "Yes",
     answer1: "No" 
-  }
+  },
   {
     id: 2,
     answer0: "Yes",
     answer1: "No"
-  }
+  },
   {
     id: 3,
     answer0: "I'm willing to tackle behavioral issues such as anxiety and leash aggression",
     answer1: "Some house-training and basic commands are fine",
     answer2: "I'd prefer to not bother with training"
-  }
+  },
   {
     id: 4,
     answer0: "0-10 hours per week",
     answer1: "10-25 hours per week",
     answer2: "25-40 hours per week"
-  }
+  },
   {
     id: 5,
     answer0: "I go running most days and hiking at the weekend",
