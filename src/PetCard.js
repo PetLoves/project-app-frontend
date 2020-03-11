dd81ac4329a1cf838ffe4f3ef1d5bc64476cfd0f
Jyoti <jyoti.contact@gmail.com>
2020-03-11 19:45:17 +0000

    add state to app

--- src/PetCard.js
+++ src/PetCard.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import './PetCard.css';
-import Card from './Card.js'
+import Card from './Card.js';
 
 class PetCard extends React.Component {
-    render() {
-        console.log(this.props.pets)
+  render() {
+    console.log(this.props.pets);
 
-      return (
-        <div className="row">
-            {this.props.pets.map((animal) => {
-                return (
-                    <Card
-                        name = {animal.name}
-                        description = {animal.description}
-                    />
-                )
-                })
-            }
-        </div>
-      );
-    }
+    return (
+      <div className="row">
+        {this.props.pets.map(animal => {
+          return <Card name={animal.name} description={animal.description} />;
+        })}
+      </div>
+    );
   }
-  
+}
+
 export default PetCard;
