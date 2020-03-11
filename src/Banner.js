4dc7b59c731f9198a357853714edfcba68b8ce15
Jyoti <jyoti.contact@gmail.com>
2020-03-11 19:20:33 +0000

    change app title

--- src/Banner.js
+++ src/Banner.js
@@ -3,24 +3,20 @@ import './Banner.css';
 import githeart from './images/githeart.jpg';
 
 class Banner extends React.Component {
-    render() {
-      return (
-        <div className="row">
-          <div className="col-1">
-            <img src={githeart} />
-          </div>
-          <div className="col-9">
-            <h1>PetLovers</h1>
-          </div>
-          <div className="col-1">
-                HEART
-          </div>
-          <div className="col-1">
-                USER
-          </div>
+  render() {
+    return (
+      <div className="row">
+        <div className="col-1">
+          <img src={githeart} />
         </div>
-      );
-    }
+        <div className="col-9">
+          <h1>PetLoves</h1>
+        </div>
+        <div className="col-1">HEART</div>
+        <div className="col-1">USER</div>
+      </div>
+    );
   }
-  
+}
+
 export default Banner;
