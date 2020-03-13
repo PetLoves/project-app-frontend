677a529593aaecf5818f4e85546f78692ac5395a
Laura Miller <lmiller.spence@gmail.com>
2020-03-13 19:59:23 +0000

    Set icons in top right of website to be responsive

--- src/Banner.js
+++ src/Banner.js
@@ -12,8 +12,16 @@ class Banner extends React.Component {
         <div className="appName col-9">
           <h1>PetLoves</h1>
         </div>
-        <div className="col-1"><img src="https://img.icons8.com/cotton/64/000000/like--v1.png"/></div>
-        <div className="col-1"><img src="https://img.icons8.com/material-outlined/48/000000/user--v1.png"/></div>
+        <div className="col-1">
+          <a href="#">
+          <img src="https://img.icons8.com/cotton/64/000000/like--v1.png" class="img-responsive" id="heart" />
+          </a>
+        </div>
+        <div className="col-1">
+          <a href="#">
+          <img src="https://img.icons8.com/material-outlined/48/000000/user--v1.png"/>
+          </a>
+        </div>
       </div>
     );
   }
